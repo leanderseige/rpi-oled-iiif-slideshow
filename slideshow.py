ec20ee28f991e738f9f6d8d1dbb04af0fe809200
Leander Seige <leander@seige.name>
2021-01-30 17:43:16 +0000

    big update

--- slideshow.py
+++ slideshow.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import cv2
 import json
+import math
 
 #--------------Driver Library-----------------#
 import RPi.GPIO as GPIO
@@ -34,13 +35,13 @@ def analyze_images(image):
     faceCascadef = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_alt.xml")
     facesf = faceCascadef.detectMultiScale(
         image,
-        minNeighbors=3,
+        minNeighbors=6,
         minSize=(20, 20)
     )
     faceCascadep = cv2.CascadeClassifier("haarcascades/haarcascade_profileface.xml")
     facesp = faceCascadep.detectMultiScale(
         image,
-        minNeighbors=3,
+        minNeighbors=6,
         minSize=(20, 20)
     )
     faces=[*facesf,*facesp]
@@ -48,31 +49,57 @@ def analyze_images(image):
 
 #----------------------MAIN-------------------------#
 
-cols = ["https://iiif.manducus.net/collections/0020/collection.json","https://iiif.manducus.net/collections/0019/collection.json","https://iiif.manducus.net/collections/0008/collection.json"]
+cola = ["https://iiif.manducus.net/collections/0020/collection.json"] # MKK Dortmund
+colb = ["https://iiif.manducus.net/collections/0019/collection.json"] # Burg Posterstein
+colc = ["https://iiif.manducus.net/collections/0008/collection.json"] # Städel Museum
+cold = ["https://wellcomelibrary.org/service/collections/genres/Group%20portraits/"]
+cole = ["https://wellcomelibrary.org/service/collections/genres/Portrait%20prints/"]
+colf = ["https://wellcomelibrary.org/service/collections/genres/Portrait%20paintings/"]
 
-fn = ""
+cols = [*cola,*colb,*colc,*cold,*cole,*colf]
 
+font = ImageFont.truetype('UbuntuMono-Bold.ttf',12)
 
 try:
     def main():
-        global fn
         OLED.Device_Init()
         while True:
             for cu in cols:
                 col = requests.get(cu)
                 col = col.json()
                 for m in col["manifests"]:
-                    Display_Picture("iiiflogo128.jpg")
                     man = requests.get(m["@id"])
                     man = man.json()
-                    srv = man["sequences"][0]["canvases"][0]["images"][0]["resource"]["service"]["@id"]
-                    img = url_to_image(srv+"/full/pct:%s,/0/native.jpg"%(4))
-                    fcs = analyze_images(img)
-                    for (x, y, w, h) in fcs:
-                        fn = wget.download(srv+"/%d,%d,%d,%d/128,128/0/default.jpg" % (x*25,y*25,w*25,h*25))
-                        Display_Picture(fn)
-                        OLED.Delay(3000)    
-                        os.remove(fn)
+                    try:
+                        for c in man["sequences"][0]["canvases"]:
+                            # interlude
+                            Display_Picture("iiiflogo128.jpg")
+                            w = float(man["sequences"][0]["canvases"][0]["width"])
+                            f = int(math.ceil(w/1000))
+                            srv = c["images"][0]["resource"]["service"]["@id"]
+                            # analyzing
+                            if os.path.exists('temp.jpg'):
+                                os.remove('temp.jpg')
+                            wget.download(srv+"/full/128,128/0/default.jpg", 'temp.jpg')
+                            image = Image.open('temp.jpg')
+                            draw = ImageDraw.Draw(image)
+                            draw.text((0,8), 'Analyzing', fill = "GREEN", font = font)
+                            OLED.Display_Image(image)
+                            uti = srv+"/full/%d,/0/native.jpg" % (int(round(w/f)))
+                            print(uti)
+                            img = url_to_image(uti)
+                            fcs = analyze_images(img)
+                            os.remove('temp.jpg')
+                            for (x, y, w, h) in fcs:
+                                if os.path.exists('temp.jpg'):
+                                    os.remove('temp.jpg')
+                                wget.download(srv+"/%d,%d,%d,%d/128,128/0/default.jpg" % (x*f,y*f,w*f,h*f), 'temp.jpg')
+                                Display_Picture('temp.jpg')
+                                OLED.Delay(3000)    
+                                os.remove('temp.jpg')
+                    except Exception as e:
+                        print(e)
+                        print("Error with %s" % m["@id"])
 
 
     if __name__ == '__main__':
@@ -80,7 +107,8 @@ try:
 
 except:
     print("\r\nEnd")
-    os.remove(fn)
+    if os.path.exists('temp.jpg'):
+        os.remove('temp.jpg')
     OLED.Clear_Screen()
     GPIO.cleanup()
 
